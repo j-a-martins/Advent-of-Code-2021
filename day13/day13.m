a7d9f2c72d2c6d8ed03a6a160e0a7886597b6150
j-a-martins <jaimeafonsomartins@gmail.com>
2021-12-13 11:08:26 +0000

    Added day 13 solution

---
+++ day13/day13.m
@@ -0,0 +1,30 @@
+%% Read data
+%file = 'day13_example.txt';
+file = 'day13_data.txt';
+
+data = readmatrix(file, Delimiter = ',', OutputType = 'string');
+
+coords = single(str2double(data)); % Get coordinates
+filt = isnan(coords(:, 1));
+instruct = data(filt ,1); % Get instructions (where coords == NaN)
+coords(filt, :) = []; % Clear NaNs from coords
+
+%% Part 1
+mirror = @(x, fold) 2 * fold - x; % Define a mirror function
+for i = 1:numel(instruct) % Process all instructions
+    op = sscanf(instruct(i), 'fold along %c=%d'); % Get current instruction
+    switch char(op(1))
+        case 'x'
+            filt = coords(:, 1) > op(2);
+            coords(filt, 1) = mirror(coords(filt, 1), op(2));
+        case 'y'
+            filt = coords(:, 2) > op(2);
+            coords(filt, 2) = mirror(coords(filt, 2), op(2));
+    end
+    coords = unique(coords, 'rows'); % Clear repeated rows
+    if i == 1, disp("Part 1: There are " + height(coords) + " visible dots at first fold."), end
+end
+
+%% Part 2
+f = figure; f.Position = [500 700 300 50];
+scatter(coords(:, 1), -coords(:, 2), 'filled')
