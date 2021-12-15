b6643d61fdc35db87fd919d51531b25a024d2d20
j-a-martins <jaimeafonsomartins@gmail.com>
2021-12-15 15:29:48 +0000

    Added example assertions, reformated function inputs, updated code formatting

--- day15/day15.m
+++ day15/day15.m
@@ -1,6 +1,8 @@
-%% Read data
-file = 'day15_example.txt';
-%file = 'day15_data.txt';
+% (): Run example puzzle, (<anything>): Run personal puzzle
+function day15(~)
+%% Read data from current folder
+p = pwd; p = p(end-4:end);
+switch nargin, case 0, file = p + "_example.txt"; otherwise, file = p + "_data.txt"; end
 
 % Read data as string array -> char matrix -> single precision matrix
 data = single(char(readmatrix(file, Delimiter = "", OutputType = 'string', NumHeaderLines = 0))) - 48;
@@ -8,15 +10,19 @@ data = single(char(readmatrix(file, Delimiter = "", OutputType = 'string', NumHe
 %% Part 1
 [P, d] = shortest_path_tl_br(data);
 disp("Part 1: The lowest total risk is " + d + " on path (" + strjoin(P,')->(') + ")")
+if ~nargin, assert(d == 40, "Part 1 is incorrect."), end
 
 %% Part 2
 [P, d] = shortest_path_tl_br(expand_cave(data, 5));
 disp("Part 2: The lowest total risk is " + d + " on path (" + strjoin(P,')->(') + ")")
+if ~nargin, assert(d == 315, "Part 2 is incorrect."), end
 
-%%
+end
+
+%% Auxiliar functions
 function [P, d] = shortest_path_tl_br(data)
 % Get source and target nodes, with edge weights
-[s, t, wt] = nodes(data);
+[s, t, wt] = get_nodes(data);
 % Create graph
 G = digraph(s, t, wt);
 % Get the shortest path from (1,1) to (h,w) and its cost
@@ -25,7 +31,7 @@ G = digraph(s, t, wt);
 %highlight(p, P, EdgeColor='r', LineWidth=2)
 end
 
-function [s, t, wt] = nodes(data)
+function [s, t, wt] = get_nodes(data)
 w = width(data); h = height(data);
 s = string.empty; t = string.empty; wt = single.empty;
 for i = 1:h
@@ -58,7 +64,7 @@ function data = expand_cave(data, exp_factor)
 w = width(data); h = height(data);
 % Replicate data exp_factor by exp_factor
 data = repmat(data, exp_factor);
-% Add 1 by Manhattan distance across replicates 
+% Add 1 by Manhattan distance across replicates
 for i = 1:(exp_factor * h)
     for j = 1:(exp_factor * w)
         data(i,j) = data(i,j) + floor((i-1)/h) + floor((j-1)/w);
