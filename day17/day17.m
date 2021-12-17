57ed4f484ffde25a7298faff6d0b75fa32370d4f
j-a-martins <jaimeafonsomartins@gmail.com>
2021-12-17 16:02:54 +0000

    Added assertions

--- day17/day17.m
+++ day17/day17.m
@@ -10,7 +10,7 @@ f_id = fopen(file, 'r'); data = fscanf(f_id, 'target area: x=%d..%d, y=%d..%d', 
 
 g_max_y = 0; % Store the global max y for Part 1
 c_solutions = 0; % Solution counter
-for v0_x = floor((sqrt(8*data(1)+1)-1)/2):data(2) % From v0_x*(v0_x+1)/2 >= xmin to x_max
+for v0_x = floor((sqrt(8*data(1)+1) - 1)/2):data(2) % From v0_x*(v0_x+1)/2 >= xmin to x_max
     for v0_y = data(3):-data(3) % From y_min to -y_min
         max_y = 0;
         t = 0;
@@ -33,15 +33,22 @@ for v0_x = floor((sqrt(8*data(1)+1)-1)/2):data(2) % From v0_x*(v0_x+1)/2 >= xmin
     end
 end
 
+%% Part 1
 disp("Part 1: The max y was " + g_max_y)
+if ~nargin, assert(g_max_y == 45, "Part 1 is incorrect."), end % Validate example puzzles
+if nargin, assert(g_max_y == 6786, "Part 1 is incorrect."), end % Validate personal puzzle
+
+%% Part 2
 disp("Part 2: There are " + c_solutions + " solutions")
+if ~nargin, assert(c_solutions == 112, "Part 2 is incorrect."), end % Validate example puzzles
+if nargin, assert(c_solutions == 2313, "Part 2 is incorrect."), end % Validate personal puzzle
 end
 
 %% Aux functions
 function [pos_x, pos_y] = find_probe_pos(v0_x, v0_y, t)
 sum_int_0_to_n = @(n) 0.5 * n .* (n + 1);
 drag = t - 1;
-vx = max(0, abs(v0_x) - drag);
+vx = max(0, abs(v0_x)-drag);
 pos_x = sign(v0_x) .* (abs(vx) .* t + min(sum_int_0_to_n(drag), sum_int_0_to_n(v0_x)));
 pos_y = v0_y .* t - sum_int_0_to_n(t-1);
 end
